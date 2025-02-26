93d0fce05d01ad6d440c2611b807e80cccd1573b
Carlos Ortega <carlosfortega2001@gmail.com>
2025-02-25 20:10:16 -0600

    updates

--- ChemSurrogate/__init__.py
+++ ChemSurrogate/__init__.py
@@ -1,13 +1,9 @@
 # src/__init__.py
-
-# This makes src a package and allows for easier imports
-
 from . import data_processing
 from . import configs
 from . import analysis
 from . import train
 from . import nn
 
-# Optional: If you want to expose specific classes/functions for easy access
 from .configs import DatasetConfig, AEConfig, EMConfig
 from .train import load_objects
